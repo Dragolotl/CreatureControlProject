98ed25a7daa174537b85675f1e3b091553475820
RamseyVincent <ravi9496@colorado.edu>
2026-04-22 11:42:39 -0600

    Removed redundant method: toString

--- src/main/java/CritterControl/Accessories/Accessory.java
+++ src/main/java/CritterControl/Accessories/Accessory.java
@@ -1,8 +1,4 @@
 package CritterControl.Accessories;
 
 public record Accessory(String name, double healthBoost, double strengthBoost, double speedBoost, double magicBoost) {
-    @Override
-    public String toString() {
-        return name;
-    }
 }
