53b6a972dafcd0bd9678bc0a1b1029d8fe362dcd
RamseyVincent <ravi9496@colorado.edu>
2026-04-25 13:51:19 -0600

    Added level based dynamic strategy

--- src/main/java/CritterControl/critters/Minitaur.java
+++ src/main/java/CritterControl/critters/Minitaur.java
@@ -2,11 +2,22 @@ package CritterControl.critters;
 
 public class Minitaur extends Critter {
     //Strength Critter
-    public Minitaur(String name, double health) {
-        super(name, health);
+    public Minitaur(String name, int level) {
+        super(name, level);
     }
 
     public Minitaur(String name){
         super(name);
+    }
+
+    @Override
+    public void setStrategyBasedOnLevel(int level) {
+        if (level < 5) {
+            strategy = strategyFactory.Level1Strategy();
+        } else if (level < 10) {
+            strategy = strategyFactory.MinitaurLevel5Strategy();
+        } else {
+            strategy = strategyFactory.MinitaurLevel10Strategy();
+        }
     }
 }
