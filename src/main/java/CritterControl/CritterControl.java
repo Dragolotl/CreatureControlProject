ed4d79874440f9c570ef9cf86e8a1a6d68997aa3
RamseyVincent <ravi9496@colorado.edu>
2026-04-25 13:50:37 -0600

    Added arena support

--- src/main/java/CritterControl/CritterControl.java
+++ src/main/java/CritterControl/CritterControl.java
@@ -2,8 +2,12 @@ package CritterControl;
 
 import CritterControl.Commands.ICommand;
 import CritterControl.critters.Critter;
+import CritterControl.critters.CritterType;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class CritterControl {
     //Might need another class to handle these to be in compliance with Single Responsibility Principle
@@ -13,6 +17,7 @@ public class CritterControl {
     private int food;
     private Boolean playing;
     private final CritterCorral corral;
+    private final Map<CritterType, Integer> arenaLevels = new ConcurrentHashMap<>();
     //need a scanner
 
     //this will have the selectAction method, and also will have a feed method
@@ -32,46 +37,7 @@ public class CritterControl {
     } //Also potentially code in an Arena if we do Ramsey's plan
 
     public void play() {
-        Critter currentCritter = new Critter();
-
-        while (playing) {
-            //Print menu somehow
-            ICommand command = selectAction(currentCritter);
-            command.execute();
-        }
-    }
-
-    public ICommand selectAction(Critter currentCritter) {
-        //Take in user input and choose the action accordingly
-        //switch (user input):
-        //1. Change Critter (not a command, just variable swapping)
-        //      print out critter list from corral
-        //      scan to get user choice
-        //      currentCritter = corral.get(userChoice);
-        //      return commandFactory.SleepCommand();
-        //2. Battle
-        //      print out arenas
-        //      get user input for choice
-        //      return commandFactory.BattleCommand(currentCritter, critterFactoryStuff)
-        //3. Feed
-        //      scan for user input for amount of food
-        //      return commandFactory.FeedCommand(currentCritter, foodAmount);
-        //4. Dress
-        //      if wardrobe is empty,
-        //          be sassy
-        //          return commandFactory.sleepCommand();
-        //      else
-        //          print out accessory list
-        //          scan for user input
-        //          Accessory accessory = corral.getWardrobe().get(user input);
-        //          return commandFactory.WearCommand(currentCritter, corral, accessory);
-        //5. Quit
-        //      return commandFactory.QuitCommand(playing);
-        //Default. maybe make them choose again...
-        return null;
-    }
-
-    public void play() {
+        initializeArenas();
         Critter currentCritter = new Critter();
 
         while (playing) {
@@ -137,6 +103,12 @@ public class CritterControl {
         if (food <= 0) {
             food = 0;
         }
+    }
+
+    public void initializeArenas() {
+        arenaLevels.put(CritterType.STRENGTH, 1);
+        arenaLevels.put(CritterType.SPEED, 1);
+        arenaLevels.put(CritterType.MAGIC, 1);
     }
 
     //have a play method
