18546e9607cfad33d058ae465732d0d98a0e67a4
Maggie <mamu5358@colorado.edu>
2026-04-25 13:38:16 -0600

    CritterControl & CritterCorral has more progress, CommandFactory is only partly implemented(I would like help)

--- src/main/java/CritterControl/Commands/CommandFactory.java
+++ src/main/java/CritterControl/Commands/CommandFactory.java
@@ -1,5 +1,15 @@
 package CritterControl.Commands;
 
-public class CommandFactory {
+import CritterControl.Accessories.Accessory;
+import CritterControl.CritterCorral;
+import CritterControl.critters.Critter;
 
+public class CommandFactory {
+    public ICommand newWearCommand(Critter critter, CritterCorral corral, Accessory accessory) {
+        return new WearCommand(critter, corral, accessory);
+    }
+
+    public ICommand newQuitCommand(Boolean isPlaying) {
+        return new QuitCommand(isPlaying);
+    }
 }
