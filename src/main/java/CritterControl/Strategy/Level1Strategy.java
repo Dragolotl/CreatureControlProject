8032736f594fffe2835a8af97a0541e6c67c68e4
RamseyVincent <ravi9496@colorado.edu>
2026-04-25 20:43:19 -0600

    Added stun to the method.

--- src/main/java/CritterControl/Strategy/Level1Strategy.java
+++ src/main/java/CritterControl/Strategy/Level1Strategy.java
@@ -7,9 +7,8 @@ public class Level1Strategy implements IStrategy {
     protected boolean dodged = false;
     protected int necrobonesDamageReduction = 0;
 
-    public void setStunned(boolean stunState) {
-        isStunned = stunState;
-    }
+    public void tryStun(Critter opponent) { opponent.getStrategy().setStunned(false); }
+    public void setStunned(boolean stunned) {isStunned = stunned;}
     public boolean isStunned() { return isStunned; }
     public void setDodged(boolean wasAttacked) { dodged = wasAttacked; }
     public boolean dodged() { return dodged; }
