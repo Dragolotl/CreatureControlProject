33aeda862002bad1c11f91b60dadfa414aa2f9ce
RamseyVincent <123579527+RamseyVincent@users.noreply.github.com>
2026-04-26 15:16:18 -0600

    Update CommandFactory.java

--- src/main/java/CritterControl/Commands/CommandFactory.java
+++ src/main/java/CritterControl/Commands/CommandFactory.java
@@ -5,8 +5,8 @@ import CritterControl.critters.Critter;
 import CritterControl.critters.CritterType;
 
 public class CommandFactory {
-    public ICommand BattleCommand(Critter critter1, int arenaLevel, CritterType opponentType, CritterCorral corral) {
-        return new BattleCommand(critter1, arenaLevel, opponentType, corral);
+    public ICommand BattleCommand(Critter critter1, CritterType opponentType, CritterCorral corral) {
+        return new BattleCommand(critter1, opponentType, corral);
       
         public ICommand newWearCommand(Critter critter, CritterCorral corral, Accessory accessory) {
             return new WearCommand(critter, corral, accessory);
