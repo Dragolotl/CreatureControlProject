bc844f3df5b960cdce511aae906157d1f7883ed6
BelleVanVliet <143553002+BelleVanVliet@users.noreply.github.com>
2026-04-25 21:52:37 -0600

    basic tree and garden setup

--- src/main/java/CritterControl/Garden/Garden.java
+++ src/main/java/CritterControl/Garden/Garden.java
@@ -1,4 +1,20 @@
 package CritterControl.Garden;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Garden {
+    private final List<Tree> trees = new ArrayList<>();
+    public void addTree(Tree tree){
+        trees.add(tree);
+    }
+    public List<Tree> getTrees(){
+        return trees;
+    }
+    public void growAllTrees(){
+        for (Tree tree : trees) {
+            tree.grow();
+        }
+    }
+    // harvest All Trees too right?
 }
--- src/main/java/CritterControl/Garden/Tree.java
+++ src/main/java/CritterControl/Garden/Tree.java
@@ -7,8 +7,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Tree {
+    private static final int GROWTH_RATE = 1;
+    private static final int TURNS_TO_GROW = 3;
+    // could potentially add max food, per tree
+    // maybe there's a maximum number of trees you can harvest from between combats,
+    // if we had more time we could've implemented a time system but that's way beyond what we have
     private final String name;
     private final Food foodType;
+    private int growCounter = 0;
     private int foodGrown = 0; // all trees start with no food
     private final FoodFactory foodFactory;
 
@@ -27,7 +33,12 @@ public class Tree {
 
 
     public void grow(){
-        foodGrown+=1;
+        // can make more complex later, maybe each tree has a different amount of turns to grow
+        // also can add randomness later
+        if (growCounter >= TURNS_TO_GROW) {
+            foodGrown += GROWTH_RATE;
+            growCounter = 0;
+        }
     }
 
     public List<Food> harvest(){
